aad827f026ce8b7c583578fc86c4978fc09cf621
HuyLemm <121721803+HuyLemm@users.noreply.github.com>
2023-02-27 21:05:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "Người yêu Paoooi",
     introTitle: 'Gửi Paoooi yêu dấu của anhhhh',
-    introDesc: `Em thích nghe nhạc vì lời
-Còn anh lại thích trọn đời có emm. 
-Chúc ngiu của anhhh có một 8/3 thật là zui zẻ nhóooo`,
+    introDesc: `Em thích nghe nhạc vì lời, còn anh lại thích trọn đời có emm. 
+Chúc ngiu của anhhh có một 8/3 thật là zui zẻ nhóooo 💙💙💙`,
     btnIntro: 'Hihi :>>',
     title: 'Gửi em công chúa bé nhỏ của anhhh 🥰',
     desc: `Nhân ngày 8/3 này, anh chúc em bé nhà mình sẽ có một ngày 8/3 thật là đáng nhớ và 
-    hạnh phúc cùng những người thân yêu nhó. Anh mong rằng sau này chúng ta sẽ có thật nhiều kỷ niệm đẹp 
-    và sống thật là vui vẻ cùng nhau <3333. Người yêu anh thích anh làm những bất ngờ như này hongg  `,
-    btnYes: 'Thích lắm <33',
+    hạnh phúc cùng những người thân yêu nhó. Anh mong rằng
+    sau này chúng ta sẽ có thật nhiều kỷ niệm đẹp 
+    và sống thật là vui vẻ cùng nhau 💗. Người yêu anh thích anh làm những bất ngờ như này hongg 💌 `,
+    btnYes: 'Thích lắm 💓',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anhhhh <3',
-    btnReply: 'Gửi cho alzinn <3',
-    reply: 'Tại vì em vô cùng yêu anhhh, hihi <333',
+    btnReply: 'Gửi cho Alzinn <3',
+    reply: 'Tại vì em vô cùng yêu anhhh, hong đc ghi cái gì khác đâu hihi 💘',
     mess: 'Anh biết mà 🥰. Yêu em nhiều lắmm 😘😘',
     messDesc: 'Trưa nay 11h30, anh qua đón đi chơi nhaaa.',
     btnAccept: 'Okiiiii lun <3',
